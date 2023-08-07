00bcdb1fb18e73bea8a3ae56621aa651cc884a14
JOSHUA <40774040+josh-ddev@users.noreply.github.com>
2023-08-07 04:12:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 'use strict';
 const container = document.querySelector('.container');
-const item0 = document.querySelector('.item--0');
-const item1 = document.querySelector('.item--1');
-const item2 = document.querySelector('.item--2');
-const item3 = document.querySelector('.item--3');
-const item4 = document.querySelector('.item--4');
-const item5 = document.querySelector('.item--5');
-const item6 = document.querySelector('.item--6');
-const item7 = document.querySelector('.item--7');
-const item8 = document.querySelector('.item--8');
+const cells = Array.from(document.querySelectorAll('.container > div'));
 const newGame = document.querySelector('.new--game');
-const p = document.querySelector('p');
-let cells = [item0, item1, item2, item3, item4, item5, item6, item7, item8];
+const remark = document.querySelector('.remark');
+const playWith = document.querySelector('.play--with');
 let gameBoard = ['', '', '', '', '', '', '', '', ''];
 const winningConditions = [
   [0, 1, 2],
@@ -24,35 +16,84 @@ const winningConditions = [
   [2, 5, 8],
 ];
 let turn = 'X';
-let won;
+let won = false;
 let playing = true;
-container.addEventListener('click', e => {
-  const clicked = e.target;
-  cells.forEach(cell => {
-    if (clicked === cell && cell.textContent === '') {
-      if (playing) {
-        console.log(cell);
-        p.textContent = '';
-        cell.textContent = turn;
-        let cellIndex = cells.indexOf(cell);
-        gameBoard[cellIndex] = turn;
-        console.log(gameBoard);
-        if (gameBoard.every(board => board !== '')) {
-          p.textContent = 'DRAW';
+let clicked;
+let cellIndex;
+
+playWith.addEventListener('click', e => {
+  if (e.target.classList.contains('player')) {
+    newGame.style.display = 'block';
+    playWith.style.display = 'none';
+    container.style.display = 'grid';
+    container.addEventListener('click', e => {
+      clicked = e.target;
+      cells.forEach(cell => {
+        if (clicked === cell && cell.textContent === '') {
+          if (playing) {
+            remark.textContent = '';
+            cell.textContent = turn;
+            cellIndex = cells.indexOf(cell);
+            gameBoard[cellIndex] = turn;
+            if (gameBoard.every(board => board !== '')) {
+              remark.textContent = 'DRAW';
+            } else {
+              win(turn);
+              turn === 'X' ? (turn = 'O') : (turn = 'X');
+              if (!won) remark.textContent = `${turn}'s turn`;
+            }
+          } else if (clicked === cell && cell.textContent !== '') {
+            remark.textContent =
+              'Cell already picked, please pick another cell';
+          }
         }
-        win(turn);
-        turn === 'X' ? (turn = 'O') : (turn = 'X');
-        if (!won) p.textContent = `${turn} turn`;
-      } else if (clicked === cell && cell.textContent !== '') {
-        p.textContent = 'Cell already picked, please pick another cell';
-      }
-    }
-  });
+      });
+    });
+  }
+  if (e.target.classList.contains('comp')) {
+    newGame.style.display = 'block';
+    playWith.style.display = 'none';
+    container.style.display = 'grid';
+    if (turn === 'X') remark.textContent = 'Your Turn';
+    container.addEventListener('click', e => {
+      clicked = e.target;
+      cells.forEach(cell => {
+        if (cell === clicked && cell.textContent === '') {
+          if (playing) {
+            cell.textContent = turn;
+            cellIndex = cells.indexOf(cell);
+            gameBoard[cellIndex] = turn;
+            if (gameBoard.every(board => board !== '')) {
+              playing = false;
+              remark.textContent = 'DRAW';
+            }
+            win(turn);
+            turn === 'X' ? (turn = 'O') : (turn = 'X');
+            if (turn === 'O' && !won && gameBoard.some(board => board === '')) {
+              remark.textContent = 'Wait Computer is thinking...';
+              setTimeout(computer, 1000);
+            }
+          }
+        }
+      });
+    });
+  }
 });
+
+newGame.addEventListener('click', () => {
+  cells.forEach(cell => (cell.textContent = ''));
+  cells.forEach(cell => (cell.style.backgroundColor = ''));
+  gameBoard = ['', '', '', '', '', '', '', '', ''];
+  remark.textContent = '';
+  playing = true;
+  won = false;
+  turn = 'X';
+});
+
 let win = function (turn) {
   winningConditions.forEach(win => {
     if (win.every(el => gameBoard[el] === turn)) {
-      p.textContent = `Player ${turn} wins`;
+      remark.textContent = `Player ${turn} wins`;
       won = true;
       playing = false;
       gameBoard = ['', '', '', '', '', '', '', '', ''];
@@ -61,12 +102,20 @@ let win = function (turn) {
   });
 };
 
-newGame.addEventListener('click', () => {
-  cells.forEach(cell => (cell.textContent = ''));
-  cells.forEach(cell => (cell.style.backgroundColor = ''));
-  gameBoard = ['', '', '', '', '', '', '', '', ''];
-  p.textContent = '';
-  playing = true;
-  won = false;
+let random;
+let computer = function () {
+  remark.textContent = '';
+  if (cells.every(cell => cell.textContent !== '')) {
+    playing = false;
+    return;
+  }
+  do {
+    random = Math.trunc(Math.random() * 9);
+  } while (cells[random].textContent !== '');
+  cells[random].textContent = 'O';
+  gameBoard[random] = 'O';
+  win(turn);
   turn = 'X';
-});
+  if (turn === 'X' && playing) remark.textContent = 'Your Turn';
+};
+
